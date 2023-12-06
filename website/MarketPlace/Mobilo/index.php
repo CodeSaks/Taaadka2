c37e8dc060e6376c0b0416fc702480f2de4380b9
Saksham <sakshammahajan243@gmail.com>
2023-12-05 17:30:22 -0800

    final marketpalchhe

--- website/MarketPlace/Mobilo/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h2>PHP is Fun, This is Prabhat site!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP! Hurray!!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-?>
